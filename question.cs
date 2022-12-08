a1125c1bf74ead9bee40242601ca64fb823496f2
LouisMaupas <louismaupas@gmail.com>
2022-12-08 16:58:51 +0100

    add instantiation

--- question.cs
+++ question.cs
@@ -13,9 +13,10 @@ class King : Piece, ICastling // classe King qui hérite de Piece et implémente I
     public string MovementDescription { get; set; }
     public string CastlingDescription { get; set; } // à l'instanciation décrire comment s'effectue le rock selon si on fait un petit ou un grand roque et selon si on joue blanc ou noir.
 
-    public King(string Move)
+    public King(string move, string castlingDescription )
     {
-        Move = "une case à la fois, dans toutes les directions.";
+        move = "une case à la fois, dans toutes les directions.";
+        CastlingDescription = castlingDescription;
     }
 
     public override string MovementDescription() // King implémente la méthode MovementDescription de Piece grâce au mot-clé override
@@ -46,4 +47,10 @@ class Queen : Piece // classe Queen qui hértie de Piece
     // On implémente pas l'interface car la reine ne peut pas roquer
 
 }
+
+
+King whiteKing = new King("Petit roque : e1 -> g1. Grand roque : e1 -> c1");
+King blackKing = new King("Petit roque : e8 -> c8. Grand roque : e8 -> g8");
+Queen blackQueen = new Queen();
+Queen whiteQueen = new Queen();
 
