cad6b93f7875fd945f20dbd9448c113d80311ce5
LouisMaupas <louismaupas@gmail.com>
2022-12-08 17:14:41 +0100

    fix: typo

--- question.cs
+++ question.cs
@@ -1,3 +1,9 @@
+King whiteKing = new King("Petit roque : e1 -> g1. Grand roque : e1 -> c1");
+King blackKing = new King("Petit roque : e8 -> c8. Grand roque : e8 -> g8");
+Queen blackQueen = new Queen();
+Queen whiteQueen = new Queen();
+
+
 abstract class Piece // classe abstraite déclaré avec abstract
 {
     public abstract string MovementDescription(); // méthode abstraite qui décrit le mouvement
@@ -10,7 +16,7 @@ interface ICastling // interface déclaré avec abstract
 
 class King : Piece, ICastling // classe King qui hérite de Piece et implémente ICastling
 {
-    public string MovementDescription { get; set; }
+    public string Move { get; set; }
     public string CastlingDescription { get; set; } // à l'instanciation décrire comment s'effectue le rock selon si on fait un petit ou un grand roque et selon si on joue blanc ou noir.
 
     public King(string move, string castlingDescription )
@@ -32,7 +38,7 @@ class King : Piece, ICastling // classe King qui hérite de Piece et implémente I
 
 class Queen : Piece // classe Queen qui hértie de Piece
 {
-    public string MovementDescription { get; set; }
+    public string Move { get; set; }
 
     public Queen(string Move)
     {
@@ -45,12 +51,6 @@ class Queen : Piece // classe Queen qui hértie de Piece
     }
 
     // On implémente pas l'interface car la reine ne peut pas roquer
-
 }
 
-
-King whiteKing = new King("Petit roque : e1 -> g1. Grand roque : e1 -> c1");
-King blackKing = new King("Petit roque : e8 -> c8. Grand roque : e8 -> g8");
-Queen blackQueen = new Queen();
-Queen whiteQueen = new Queen();
 
